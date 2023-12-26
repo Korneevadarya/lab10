c1a1631cd70d7c1cd4fcf9a922605d9bee44fff7
Korneevadarya <145188070+Korneevadarya@users.noreply.github.com>
2023-12-26 15:08:51 +0500

    Delete lab9.py

--- lab9.py
+++
@@ -1,7 +0,0 @@
-from itertools import product
-
-def unique_combinations(*sequences):
-    for combination in product(*sequences):
-        unique_combination = (combination)
-        yield unique_combination
-
