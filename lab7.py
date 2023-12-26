86c0e75ce24c5eff1c067a4227f662c4277ccd0a
Korneevadarya <145188070+Korneevadarya@users.noreply.github.com>
2023-12-26 15:08:30 +0500

    Delete lab7.py

--- lab7.py
+++
@@ -1,8 +0,0 @@
-#Функция для расчета a[i] = a[i-2] + ((a[i-1])/(2^(i-1))) с использованием рекурсии:
-
-def calculate_a_recursive(i):
-    if i == 0 or i == 1:
-        return 1
-    else:
-        return calculate_a_recursive(i-2) + (calculate_a_recursive(i-1)/(2**(i-1)))
-
